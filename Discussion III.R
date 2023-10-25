168a95046c4dc538853bee9ae4fe38bc143dcaf0
DapperDataAnalyst <112096821+DapperDataAnalyst@users.noreply.github.com>
2023-10-25 16:26:41 -0500

    Add files via upload

--- Discussion III.R
+++ Discussion III.R
@@ -1,5 +1,4 @@
 library(rstanarm)
-library(dplyr)
 cows <- read.table(file = 'https://raw.githubusercontent.com/DapperDataAnalyst/Causal_Inference/main/Cows.txt', header = T, sep = "\t", fileEncoding = "UTF-8")
 
 # Part 1)
@@ -11,38 +10,24 @@ model2 <- stan_glm(milk*fat ~ level+lactation+age+initial.weight, data = cows, r
 summary(model2)
 
 # Part 3)
-# Convert 'level' to a factor with four levels
-cows$level <- as.factor(cows$level)
 
 # Fit the stan_glm model with 'level' as a categorical predictor
-model2 <- stan_glm(milk*fat ~ level + lactation + age + initial.weight, data = cows, refresh = 0)
-
-# Get posterior samples of the coefficients
-posterior_samples <- as.matrix(model2)
-
-# Get the point estimates for the coefficient of 'level' for each level
-point_estimates <- data.frame(
-  Level = c(0.1, 0.2, 0.3),
-  Coefficient = apply(posterior_samples[, grepl("level", colnames(posterior_samples))], 2, mean),
-  StnDev = apply(posterior_samples[, grepl("level", colnames(posterior_samples))], 2, sd)
-)
-
-point_estimates <- point_estimates %>% mutate(lower = Coefficient - 2*StnDev) %>%
-  mutate(upper = Coefficient + 2*StnDev)
+model3 <- stan_glm(milk*fat ~ factor(level) + lactation + age + initial.weight, data = cows, refresh = 0)
+summary(model3)
 
 # Load the ggplot2 package
 library(ggplot2)
 
 # Create a sample dataset
-data <- data.frame(
-  Category = point_estimates$Level,
-  PointEstimate = point_estimates$Coefficient,
-  LowerCI = point_estimates$lower,
-  UpperCI = point_estimates$upper
+data_multi <- data.frame(
+  Category = c('Level 1','Level 2', 'Level 3'),
+  PointEstimate = c(coef(model3)[2],coef(model3)[3],coef(model3)[4]),
+  LowerCI = c(coef(model3)[2]+2*se(model3)[2],coef(model3)[3]+2*se(model3)[3],coef(model3)[4]+2*se(model3)[4]),
+  UpperCI = c(coef(model3)[2]-2*se(model3)[2],coef(model3)[3]-2*se(model3)[3],coef(model3)[4]-2*se(model3)[4])
 )
 
 # Create the multiplot
-plot <- ggplot(data, aes(x = PointEstimate, y = Category)) +
+plot <- ggplot(data_multi, aes(x = PointEstimate, y = Category)) +
   geom_linerange(aes(xmin = LowerCI, xmax = UpperCI), size = 1) +
   geom_point(size = 5) +  # Add point estimates
   labs(
